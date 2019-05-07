5f44a73a2253401bf933a2ffd3bd47a06d8fcf3f
Daniel Saunders <dsaunders@dansaunders.me>
2019-05-07 11:39:59 -0400

    More changes

--- awsumchan.js
+++ awsumchan.js
@@ -7,20 +7,21 @@ function init_nsfw()
 {
   var nsfw = localStorage.getItem('awsumchan-nsfw-toggle') || false;
 
-  if (nsfw) {
+  if (nsfw === true) {
     document.getElementById('toggle-nsfw').textContent = '[hide nsfw content]';
     document.querySelectorAll('li.nsfw').forEach(i => i.style.display = 'block');
   }
+
+  document.getElementById('toggle-nsfw').onclick = toggle_nsfw;
 }
 
 function toggle_nsfw()
 {
   var nsfw = localStorage.getItem('awsumchan-nsfw-toggle') || false;
 
-  document.getElementById('toggle-nsfw').textContent = '[' + (nsfw ? 'show' : 'hide') + ' nsfw content]';
-  document.querySelectorAll('li.nsfw').forEach(i => i.style.display = (nsfw ? 'none': 'block'));
-  localStorage.setItem('awsumchan-nsfw-toggle', !nsfw);
+  document.getElementById('toggle-nsfw').textContent = '[' + (nsfw === true ? 'show' : 'hide') + ' nsfw content]';
+  document.querySelectorAll('li.nsfw').forEach(i => i.style.display = (nsfw === true ? 'none': 'block'));
+  localStorage.setItem('awsumchan-nsfw-toggle', nsfw === true ? false : true);
 }
 
 document.onload = init_nsfw;
-document.getElementById('toggle-nsfw').onclick = toggle_nsfw;
